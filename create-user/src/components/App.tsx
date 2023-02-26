a2cb098a3a1bc7d1b00db31967bba965cba635d7
Tylor Hanshaw <tylorjhanshaw@gmail.com>
2023-02-26 17:34:39 -0500

    initial react and typescript setup and config

---
+++ create-user/src/components/App.tsx
@@ -0,0 +1,16 @@
+import { FC } from 'react';
+
+type AppPropsType = {
+  // empty props for now
+};
+
+const App: FC<AppPropsType> = (props) => {
+
+  return (
+    <>
+      Testing
+    </>
+  );
+};
+
+export default App;
